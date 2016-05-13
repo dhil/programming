039f2a5a2c6a7c9aa2bbf4e6179b6d99cede86d2
Daniel Hillerström <daniel.hillerstrom@ed.ac.uk>
2016-05-14 00:13:02 +0100

    Making Change dynamic programming problem

--- dynamic/MakingChange.hs
+++ dynamic/MakingChange.hs
@@ -7,11 +7,30 @@
 -- Let M(j) be the minimum number of coins required to make change for
 -- amount of money j.  Equation: M(j) = min_i {M(j - v_i)} + 1
 
-denominations :: [Integer]
+-- Solution adapted from https://www.schoolofhaskell.com/school/to-infinity-and-beyond/pick-of-the-week/coinChange
+
+import Data.Array
+
+type Coin = Integer
+
+denominations :: [Coin]
 denominations = [1,5,10,20,50,100]
 
-change :: Integer -> Integer
-change n
-  | n <= 0 = 0
-  | n `elem` denominations = n
-change j = minimum $ filter (>0) $ map (\v -> 1 + change (j - v)) denominations
+change :: [Coin] -> Integer -> Maybe (Integer, [Coin])
+change coins amount = get numCoins amount
+     where
+       m = array ((0,0), (numCoins, amount)) [((i,c), takeCoin i c) | i <- [0..numCoins], c <- [0..amount]]
+       get i c
+         | c < 0 || i < 0 = Nothing
+         | c == 0         = Just (0, [])
+         | otherwise      = m ! (i, c)
+       numCoins = length coins - 1
+       takeCoin i c
+         | coin > c   = get (i-1) c
+         | otherwise  = case (get i (c - coin), get (i - 1) c) of
+                          (Just (n, t), Just (n', t')) -> Just $ if n+1 <= n' then (n+1, coin : t) else (n', t')
+                          (Nothing,     Just (n', t')) -> Just (n', t')
+                          (Just (n, t), Nothing)       -> Just (n+1, coin : t)
+                          (Nothing,     Nothing)       -> Nothing
+         where coin = coins !! i
+                                               
