406fb55924dcb1ed4efcbf0eb9c5433704fd3e3e
Daniel Hillerström <daniel.hillerstrom@ed.ac.uk>
2018-01-24 16:18:59 +0000

    Array diff

---
+++ misc/missing_element.ml
@@ -0,0 +1,34 @@
+(* There is an array of non-negative integers. A second array is
+   formed by shuffling the elements of the first array and deleting a
+   random element. Given these two arrays, find which element is missing
+   in the second array. *)
+
+(* ... in other words this task asks to compute the "set difference"
+   on arrays. *)
+
+(* This solution is more general *)
+let diff : int array -> int array -> int array
+  = fun a b ->
+  let set = Hashtbl.create (Array.length b) in
+  Array.iter
+    (fun x ->
+      if Hashtbl.mem set x then ()
+      else Hashtbl.add set x ())
+    b;
+  let dset = Hashtbl.create (Array.length a) in
+  Array.iter
+    (fun x ->
+      if Hashtbl.mem set x then ()
+      else Hashtbl.add dset x ())
+    a;
+  let darr =
+    let length = Hashtbl.length dset in
+    Array.make length 0
+  in
+  let (a, _) =
+    Hashtbl.fold
+      (fun x _ (a,i) ->
+        a.(i) <- x;
+        (a, i+1))
+      dset (darr, 0)
+  in a
