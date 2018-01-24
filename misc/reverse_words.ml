4b6a45f54825b236e10860cda7bc123d5aa07356
Daniel Hillerström <daniel.hillerstrom@ed.ac.uk>
2018-01-24 18:56:58 +0000

    Reverse words

--- misc/reverse_words.ml
+++ misc/reverse_words.ml
@@ -2,6 +2,19 @@
 
 let reverse_words sentence =
   let sentence = String.trim sentence in
+  let ws_code = Char.code ' ' in
   let rec words s len i j acc =
-    if i > len then acc
-    else if s.[i]
+    if j = len then
+      String.sub s i (j - i) :: acc
+    else if Char.code s.[j] = ws_code then
+      let acc =
+        if i <> j then
+          String.sub s i (j - i) :: acc
+        else
+          acc
+      in
+      words s len (j+1) (j+1) acc
+    else
+      words s len i (j+1) acc
+  in
+  words sentence (String.length sentence) 0 0 []
