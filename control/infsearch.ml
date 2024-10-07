1bba0f368b38a9f8d2655babd9c016d8c4b86151
Daniel Hillerström <daniel.hillerstrom@ed.ac.uk>
2024-10-07 17:10:29 +0200

    Alternative implementations

--- control/infsearch.ml
+++ control/infsearch.ml
@@ -122,13 +122,33 @@ module ExplicitLazy = struct
 end
 
 (* Adapted from Bauer (https://www.youtube.com/watch?v=1j3h2vb2BRc) *)
-module Modulus = struct
+module Pruned = struct
   (* Modulus of continuity using a local reference. *)
   let mu f a =
     let r = ref Nat.zero in
     let b n = (r := Nat.max n !r; a n) in
-    ignore (f b);
-    !r
+    ignore (f b); !r
+
+  (* Modulus of continuity using exceptions & recursion. *)
+  let mu' f a =
+    let exception Abort in
+    let rec search k =
+      try
+        let b n = (if n < k then a n else raise Abort) in
+        ignore (f b); k
+      with Abort -> search (k+1)
+    in
+    search 0
+
+  (** With call/cc
+      open SMLofNJ.Cont ;
+      fun mu f x =
+        let fun search n =
+          callcc (fn k => (f (fn j => if j < n then x j else throw k (search (n+1))) ; n))
+        in
+          search 0
+        end
+   *)
 end
 
 module Eff = struct
